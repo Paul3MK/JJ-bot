92605ae7b3d3f7a779d81c8d2b4a49cb67bc8329
Paul3MK <39906601+Paul3MK@users.noreply.github.com>
2019-07-06 12:02:43 +0000

    another change for heroku

--- botServer.py
+++ botServer.py
@@ -4,7 +4,7 @@ import multiprocessing
 myProcesses = ("actionServerRun.py", "dialogueServerRun.py")
 
 def run_process(process):
-    subprocess.Popen("python", "{}".format(process))
+    subprocess.Popen(["python", "{}".format(process)])
 
 if __name__ == "__main__":
     pool = multiprocessing.Pool(processes=2)
