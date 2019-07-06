68a2b950dfb59bf17e89135e55d1761ac2e7c032
Paul3MK <39906601+Paul3MK@users.noreply.github.com>
2019-07-06 23:08:08 +0000

    subprocess change

--- dialogueServerRun.py
+++ dialogueServerRun.py
@@ -7,6 +7,7 @@ from rasa.model import get_model
 from rasa.core.channels.facebook import FacebookInput
 #from rasa.cli.run import run_actions
 from rasa.run import run as runB
+import subprocess
 
 def runIt(port):  
     logging.basicConfig(level=logging.DEBUG)
@@ -16,11 +17,12 @@ def runIt(port):
     runB(model="models", endpoints="endpoints.yml", credentials="credentials.yml", port=port_value)
     agent = Agent.load(unpacked_model, interpreter=RegexInterpreter())
 
-    input_channel = FacebookInput(verify="JJbot", secret="ec21c7ab5f81ea576a043ce42da28846", page_access_token="EAAWtJ6GdnowBAAQvgHd9BHPRmqvzBfUPW1UadXuUXhofveak7BvFrpXn1VGXZACYVT0X4nkarxGQ1k0CxZB4YZA4zb3ywZBqeKj0t8e4hsyUrp4DRnAA5m5inThmRThN8uJqKBDvyssu05p9tuDRZBDxEPc8h0LNZAUxXSmJKXggZDZD")
+    input_channel = FacebookInput(fb_verify="JJbot", fb_secret="ec21c7ab5f81ea576a043ce42da28846", fb_page_access_token="EAAWtJ6GdnowBAAQvgHd9BHPRmqvzBfUPW1UadXuUXhofveak7BvFrpXn1VGXZACYVT0X4nkarxGQ1k0CxZB4YZA4zb3ywZBqeKj0t8e4hsyUrp4DRnAA5m5inThmRThN8uJqKBDvyssu05p9tuDRZBDxEPc8h0LNZAUxXSmJKXggZDZD")
     agent.handle_channels([input_channel], port_value, serve_forever=True)
 
     
 
 # if __name__ == "__main__":
 assigned_port = int(os.environ.get("PORT", 5000)) #port assigned by  Heroku
+subprocess.Popen(["python", "actionServerRun.py"])
 runIt(assigned_port)
