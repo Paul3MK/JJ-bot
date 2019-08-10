525f61b2da8f0cd9f93862792a0b0644f4ab4499
Paul3MK <39906601+Paul3MK@users.noreply.github.com>
2019-08-10 22:39:09 +0000

    counter to check bot training and db provision

---
+++ counterCreator.py
@@ -0,0 +1,21 @@
+import os
+import psycopg2
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+#con = psycopg2.connect(database="jjtestdb", user="postgres", password="hieg") #for local work
+con =  psycopg2.connect(DATABASE_URL, sslmode='require')
+with con:
+
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS trained_counter")
+    cur.execute("CREATE TABLE trained_counter(counter CHAR(1))")
+    cur.execute("INSERT INTO trained_counter VALUES(0)")
+    # cur.execute("SELECT * FROM trained_counter")
+    # count = str(cur.fetchall())
+    
+    # if '0' in count:
+    #     print("success")
+    # else:
+    #     print("fail")
+
